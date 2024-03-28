13f6fec8f65ed4d522781cd77cd20c96f31614fe
Noha Gad <nohaahed209@gmail.com>
2024-03-29 00:10:45 +0100

    Call wait_for_db command when running server

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
 
         while db_up is False:
             try:
-                self.check(database=['default'])
+                self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Waiting for database ...')
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -22,7 +22,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -33,4 +33,5 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
+        patched_check.assert_called_with(databases=['default'])
         self.assertEqual(patched_check.call_count, 6)
