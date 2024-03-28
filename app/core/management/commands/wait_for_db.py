b397fd0177c9a9142e5bb0a6a0ba1138002084cd
Noha Gad <nohaahed209@gmail.com>
2024-03-28 04:28:38 +0100

    Add wait for db command and it's unit test

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+"""
+Django command to wait for the database to be available.
+"""
+from django.core.management.base import BaseCommand
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+import time
+
+
+class Command(BaseCommand):
+    """ Django command to wait for database. """
+
+    def handle(self, *args, **options):
+        """ Command to handle wait for database untill it is ready. """
+
+        self.stdout.write('Checking if database is up ...')
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(database=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Waiting for database ...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database Available!'))
