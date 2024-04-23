5ae3be640c75a58d3b367bc8671bf0808d37b719
Noha Gad <nohaahed209@gmail.com>
2024-04-23 17:43:30 +0200

    Added health check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,9 +21,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
